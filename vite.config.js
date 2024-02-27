f54159caa406ba1b3375e82263e676ecb79e4352
Abhay Hitendra Dusane <99988396+CallmeAbhy@users.noreply.github.com>
2024-02-27 22:26:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PortFolio/', // Adjust this to match your GitHub Pages URL
-  plugins: [react()]
+  plugins: [react()],
+  base: "/PortFolio/",
+  server: {
+    host: "0.0.0.0",
+    port: "3000",
+  },
 });
